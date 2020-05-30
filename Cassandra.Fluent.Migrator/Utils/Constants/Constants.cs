e8b4cf961ca664df0199865bbe0a25409e508504
Youssef Benh <youssef.benhessou@gmail.com>
2020-05-30 01:49:32 -0400

    finished the first iteration for the base method/actions needed for the project

---
+++ Cassandra.Fluent.Migrator/Utils/Constants/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Cassandra.Fluent.Migrator.Utils.Constants
+{
+    public static class Constants
+    {
+        internal const string PRIMARY_KEY_ATTRIBUTE = "PartitionKeyAttribute";
+    }
+}
