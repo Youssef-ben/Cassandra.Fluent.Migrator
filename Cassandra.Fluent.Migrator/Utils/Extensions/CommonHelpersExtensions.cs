9ae0533d8babc1dcfeab97adb4ec0567b9f13879
Youssef Benh <youssef.benhessou@gmail.com>
2020-05-30 01:49:32 -0400

    finished the first iteration for the base method/actions needed for the project

---
+++ Cassandra.Fluent.Migrator/Utils/Extensions/CommonHelpersExtensions.cs
@@ -0,0 +1,68 @@
+﻿namespace Cassandra.Fluent.Migrator.Utils.Extensions
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using Microsoft.Rest.ClientRuntime.Azure.Authentication.Utilities;
+
+    internal static class CommonHelpersExtensions
+    {
+        private static CultureInfo DEFAULT_CUTURE => new CultureInfo("en-US", false);
+
+        /// <summary>
+        ///  Returns a new string whose textual value is the same as this string,
+        ///  but whose binary representation is in Unicode normalization form C
+        ///  then return a lowercase form.
+        /// </summary>
+        /// <param name="self">The string to be formatted.</param>
+        /// <returns>Normalized string.</returns>
+        internal static string NormalizeString([NotNull]this ColumnTypeCode self)
+        {
+            Check.NotNull(self);
+
+            return self.ToString().Normalize().ToLower(DEFAULT_CUTURE);
+        }
+
+        /// <summary>
+        ///  Returns a new string whose textual value is the same as this string,
+        ///  but whose binary representation is in Unicode normalization form C
+        ///  then return a lowercase form.
+        /// </summary>
+        /// <param name="self">The string to be formatted.</param>
+        /// <returns>Normalized string.</returns>
+        internal static string NormalizeString([NotNull]this string self)
+        {
+            Check.NotEmptyNotNull(self);
+
+            return self.Trim().Normalize().ToLower(DEFAULT_CUTURE);
+        }
+
+        /// <summary>
+        /// Format and returns a new string whose textual value is the same as this string,
+        /// but whose binary representation is in Unicode normalization form C
+        /// then return a lowercase form.
+        /// Format and normalize the given string based on the arguments passed to it.
+        /// </summary>
+        /// <param name="self">The string object calling this method.</param>
+        /// <param name="args">The target string values.</param>
+        /// <returns>Returns normalized and formatted text.</returns>
+        internal static string NormalizeString([NotNull]this string self, params string[] args)
+        {
+            Check.NotEmptyNotNull(self);
+
+            return string.Format(self, args).NormalizeString();
+        }
+
+        /// <summary>
+        /// Validate that this string contains the targeted value.
+        /// Checks if the string object calling this method contains the targeted value.
+        /// </summary>
+        /// <param name="self">The string object calling this method.</param>
+        /// <param name="targetText">The target string to look up.</param>
+        /// <param name="args">The target string arguments.</param>
+        /// <returns>True if value exists. False otherwise.</returns>
+        internal static bool ContainsText(this string self, string targetText, params string[] args)
+        {
+            return self.NormalizeString().Contains(targetText.NormalizeString(args));
+        }
+    }
+}
---
+++ Docs/example.md
@@ -0,0 +1,53 @@
+```CSharp
+// Should Check for Schema Agreement before going to next statement
+public class AddFieldsToTableXXXX: IMigrator{
+    private FluentCassandraMigrator migrator;
+
+    public AddFieldsToTableXXXX(FluentCassandraMigrator migrator){
+        this.migrator = migrator;
+    }
+
+    public string Name => This.GetType().Name;
+    public Version Version => new Version ("1.0.0");
+    public string description => "Migration to add fields to table xxxx";
+
+    public void ApplyMigrationAsync(){
+        // Migration for Tables
+        this.migrator.AddColumnToTableAsync(table, field, type);
+        this.migrator.AddColumnToTableAsync(table, field);
+
+        this.migrator.AddColumnToTableAsync<Table>(field, type);
+        this.migrator.AddColumnToTableAsync<Table>(field);
+
+        // Migration for UDTs
+        this.migrator.AddColumnToUdtAsync(udt, field, type);
+        this.migrator.AddColumnToUdtAsync(udt, field);
+
+        this.migrator.AddColumnToUdtAsync<Udt>(field, type);
+        this.migrator.AddColumnToUdtAsync<Udt>(field);
+    }
+}
+```
+
+
+```CSharp
+public void ConfigureServices(IServiceCollection services)
+{
+	...
+	// Should Configure the Cassandra Session before.
+	...
+	seviceProvider
+		.AddSingleton<IMigrator, AddFieldsToTableXXXX>();
+	...
+}
+
+public void Configure(IApplicationBuilder app)
+{
+	...
+	// Should be declared at the end.
+	// Will run the `CassandraMigrator.Migrate()`
+	app.UseCassandraMigration();
+}
+...
+```
+
