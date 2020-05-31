c683f4a5bc3efe380fb38ae8ecb4ceefce8aa74f
Youssef Benh <youssef.benhessou@gmail.com>
2020-05-31 16:46:05 -0400

    Added Network

--- Makefiles/Cassandra.mk
+++ Makefiles/Cassandra.mk
@@ -9,12 +9,14 @@ DB_CONTAINER_NAME	= cfm-database
 DB_CLUSTER_NAME		= cfm-cluster
 
 DB_VOLUME			= cfm-database
+DB_NETWORK			= cfm-database
 
 ## Check common values.
 DB_CONTAINER_EXISTS	= $(shell docker ps -a --no-trunc --quiet --filter name='^$(DB_CONTAINER_NAME)$$' | wc -l | sed -e 's/^[ \t]*//')
 DB_CONTAINER_UP		= $(shell docker ps --no-trunc --quiet --filter name='^$(DB_CONTAINER_NAME)$$' | wc -l | sed -e 's/^[ \t]*//')
-
 DB_VOLUME_EXISTS	= $(shell docker volume ls --quiet --filter name='^$(DB_VOLUME)$$' | wc -l | sed -e 's/^[ \t]*//')
+
+DB_NETWORK_EXISTS	= $(shell docker netwok ls --quiet --filter name='^$(DB_NETWORK)$$' | wc -l | sed -e 's/^[ \t]*//')
 
 start-cassandra: ## Start the container if exists. Otherwise it pulls the image and create a new database container.
 
@@ -24,10 +26,17 @@ ifeq ($(DB_VOLUME_EXISTS), 0)
 	@docker volume create $(DB_VOLUME) > /dev/null;
 endif
 
+## Create the cassandra network if it doesn't exits.
+ifeq ($(DB_VOLUME_EXISTS), 0)
+	@echo "Creating database network {$(DB_NETWORK)}..."
+	@docker network create -d bridge  $(DB_NETWORK) > /dev/null;
+endif
+
 ifeq ($(DB_CONTAINER_EXISTS), 0)
 	@echo "Creating a new database container {$(DB_CONTAINER_NAME)}..."	
 	@docker run \
 		--name $(DB_CONTAINER_NAME) \
+		--network $(DB_NETWORK) \
 		-e CASSANDRA_CLUSTER_NAME=$(DB_CLUSTER_NAME) \
 		-p $(DB_PORT):9042 \
 		-v $(DB_VOLUME):/var/lib/cassandra \
@@ -48,13 +57,13 @@ endif
 stop-cassandra: ## Stops the cassandra container if it's up.
 ifeq ($(DB_CONTAINER_UP), 1)
 	@echo "Stopping the Cassandra container $(DB_CONTAINER_NAME)..."
-	@docker stop $(DB_CONTAINER_NAME)
+	@docker stop $(DB_CONTAINER_NAME) > /dev/null
 endif
 
 remove-cassandra: stop-cassandra ## Stops and remove the cassandra container
 ifeq ($(DB_CONTAINER_EXISTS), 1)
 	@echo "Removing the Cassandra container $(DB_CONTAINER_NAME)..."
-	@docker rm $(DB_CONTAINER_NAME)
+	@docker rm $(DB_CONTAINER_NAME) > /dev/null
 endif
 
 restart-cassandra: stop-cassandra start-cassandra ## Restart the cassandra container.
@@ -65,10 +74,10 @@ start-cqlsh: ## Start Cassandra {CQLSH} console to query the database.
 
 clean: remove-cassandra ## Remove the container and delete the volume and image.
 	@echo "Deleting the volume $(DB_VOLUME)..."
-	@docker volume rm $(DB_VOLUME)
+	@docker volume rm $(DB_VOLUME) > /dev/null
 
 	@echo "Deleting the cassandra image $(DB_IMAGE)..."
-	@docker rmi $(DB_IMAGE)
+	@docker rmi $(DB_IMAGE)  > /dev/null
 
 queries-example: ## Show some queries that can be used for tests.
 	@echo "List available keyspaces: DESCRIBE keyspaces;"
